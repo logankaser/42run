3b613f3e88d37cb63d77076277e22b19c3a8d056
Logan James Kaser <logankaser@fyrn.io>
2018-11-02 20:20:54 -0700

    onwards and upwards

--- main.py
+++ main.py
@@ -8,120 +8,103 @@ from OpenGL.GL import shaders
 from PIL import Image
 import glfw
 from pyrr import matrix44
+from wavefront_reader import read_wavefront
 
-VERTEX_SHADER = """
-#version 410 core
 
-layout (location=0) in vec4 position;
-layout (location=1) in vec4 color;
 
-smooth out vec4 theColor;
 
-void main()
-{
-    gl_Position = position;
-    theColor = color;
-}
-"""
+def load_models():
+    """Load models."""
+    all_vert = []
+    all_uv = []
+    all_norm = []
+    for obj_file in ["assets/test.obj"]:
+        for mesh in read_wavefront(obj_file).values():
+            all_vert.append(np.array(mesh["v"], dtype=np.float32))
+            all_uv.append(np.array(mesh["vt"], dtype=np.float32))
+            all_norm.append(np.array(mesh["vn"], dtype=np.float32))
 
-FRAGMENT_SHADER = """
-#version 410 core
+    vert = np.concatenate(all_vert).ravel()
+    uv = np.concatenate(all_uv).ravel()
+    norm = np.concatenate(all_norm).ravel()
 
-smooth in vec4 theColor;
-out vec4 outputColor;
-
-void main()
-{
-    outputColor = theColor;
-}
-"""
-
-shaderProgram = None
-VAO = None
-
-def initialize():
-    global VERTEX_SHADER
-    global FRAGMENT_SHADER
-
-    # create VAO
-    VAO = glGenVertexArrays(1)
-    glBindVertexArray(VAO)
-
-    # compile shaders and program
-    try:
-        vertexShader = shaders.compileShader(VERTEX_SHADER, GL_VERTEX_SHADER)
-        fragmentShader = shaders.compileShader(FRAGMENT_SHADER, GL_FRAGMENT_SHADER)
-        shaderProgram = shaders.compileProgram(vertexShader, fragmentShader)
-    except RuntimeError as e:
-        print(str(e).replace("\\n", "\n").replace("\\", ""))
-        exit("Shaders failed to compile")
-
-    # position
-    posData = np.array([
-            0.0, 0.5, 0.0, 1.0,
-            0.5, -0.366, 0.0, 1.0,
-            -0.5, -0.366, 0.0, 1.0,
-        ],
-        dtype=np.float32
-    )
-
-    # color
-    colorData = np.array([
-            1.0, 0.0, 0.0, 1.0,
-            0.0, 1.0, 0.0, 1.0,
-            0.0, 0.0, 1.0, 1.0,
-        ],
-        dtype=np.float32
-    )
-
-    # create VBO
-    VBO = glGenBuffers(2)
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[0])
-    glBufferData(GL_ARRAY_BUFFER, posData.nbytes, posData, GL_STATIC_DRAW)
+    # create vbo
+    vbo = glGenBuffers(3)
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0])
+    glBufferData(GL_ARRAY_BUFFER, vert.nbytes, vert, GL_STATIC_DRAW)
     glEnableVertexAttribArray(0)
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, None)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[1])
-    glBufferData(GL_ARRAY_BUFFER, colorData.nbytes, colorData, GL_STATIC_DRAW)
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1])
+    glBufferData(GL_ARRAY_BUFFER, uv.nbytes, uv, GL_STATIC_DRAW)
     glEnableVertexAttribArray(1)
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, None)
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, None)
 
-    #glBindBuffer(GL_ARRAY_BUFFER, 0)
-    #glBindVertexArray(0)
-    glUseProgram(shaderProgram)
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[2])
+    glBufferData(GL_ARRAY_BUFFER, norm.nbytes, norm, GL_STATIC_DRAW)
+    glEnableVertexAttribArray(2)
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, None)
+    return len(vert) // 3
 
 
-def render():
+def render(ctx, num_verts):
     """Render a frame."""
-    glClearColor(0, 0, 0, 1)
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glDrawArrays(GL_TRIANGLES, 0, 3)
+    ctx.clear()
+    ctx.upload_matrixes()
+    glDrawArrays(GL_TRIANGLES, 0, num_verts)
+    err = glGetError()
+    if (err != GL_NO_ERROR):
+        print("GLERROR: ", gluErrorString(err))
+        sys.exit()
 
 
-class Scene:
-    """Stores the state of the scene."""
-
-    def __init__(self, shaders=[]):
-        """Create scene."""
-        self.shaders = shaders
-
-
-class GLContext:
-    """Class to make handling OpenGL context easy."""
-
-    def __init__(self, clear_color=(0.1, 0.1, 0.12, 1)):
-        """Init GLContext."""
-        vao_id = GLuint(0)
-        glGenVertexArrays(1, ctypes.byref(vao_id))
-        glBindVertexArray(vao_id)
+class GlContext:
+    """Store GL state."""
+    def __init__(self, V, P):
+        self.V = V
+        self.P = P
+        self.program_id = None
+        self.V_id = None
+        self.VP_id = None
+        # create vao
+        vao = glGenVertexArrays(1)
+        glBindVertexArray(vao)
+        glClearColor(0.3, 0.3, 0.3, 1)
         glEnable(GL_DEPTH_TEST)
         glDepthFunc(GL_LESS)
-        cc = clear_color
-        glClearColor(cc[0], cc[1], cc[2], cc[3])
 
-    def set_clear_color(r, g, b, a):
-        """Set clear color."""
-        glClearColor(r, g, b, a)
+    def clear(self):
+        """Clear buffer."""
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    def load_shaders(self, frag, vert):
+        """Load shaders."""
+        try:
+            with open(frag, "r") as src:
+                frag_src = src.read()
+            with open(vert, "r") as src:
+                vert_src = src.read()
+        except Exception as e:
+            exit("Error reading shader from disk")
+        # compile shaders and program
+        try:
+            frag_shader = shaders.compileShader(frag_src, GL_FRAGMENT_SHADER)
+            vert_shader = shaders.compileShader(vert_src, GL_VERTEX_SHADER)
+            program_id = shaders.compileProgram(vert_shader, frag_shader)
+        except RuntimeError as e:
+            print(str(e).replace("\\n", "\n").replace("\\", ""))
+            exit("Shaders failed to compile")
+        self.program_id = program_id
+        glUseProgram(self.program_id)
+        self.V_id = glGetUniformLocation(self.program_id, "VP")
+        self.VP_id = glGetUniformLocation(self.program_id, "P")
+
+    def upload_matrixes(self):
+        """Send matrixes to the GPU."""
+        glUniformMatrix4fv(
+            self.VP_id, 1, GL_FALSE, matrix44.multiply(self.V, self.P)
+        )
+        glUniformMatrix4fv(self.V_id, 1, GL_FALSE, self.V)
 
 
 def main():
@@ -144,23 +127,21 @@ def main():
     icon = Image.open("assets/icon.png")
     glfw.set_window_icon(window, 1, [icon])
 
-    camera = matrix44.create_look_at(
+    V = matrix44.create_look_at(
         np.array([0, 0, -1]),  # position
         np.array([0, 0, 0]),  # target
         np.array([0, 1, 0])  # up vector
     )
-    print(camera)
-    project = matrix44.create_perspective_projection(80, 1, 0.1, 100)
-    print(project)
-    res = project * camera
-    print(res)
+    P = matrix44.create_perspective_projection(80, 1, 0.1, 100)
+    ctx = GlContext(V, P)
+    ctx.load_shaders("assets/42run.frag", "assets/42run.vert")
 
-    initialize()
+    num_verts = load_models()
 
     # mainloop
     while not glfw.window_should_close(window):
         glClear(GL_COLOR_BUFFER_BIT)
-        render()
+        render(ctx, num_verts)
         glfw.swap_buffers(window)
         glfw.poll_events()
 
